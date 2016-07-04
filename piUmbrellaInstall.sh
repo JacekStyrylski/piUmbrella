e29999b2f8ae65de53fd5c758b8725fdca90832b
henioStraszny <jacek.styrylski@gmail.com>
2016-07-04 20:14:06 +0200

    xx

--- piUmbrellaInstall.sh
+++ piUmbrellaInstall.sh
@@ -1,4 +1,16 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git -y
-sudo apt-get install mc -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install \
+mc \
+git \
+libffi-dev \
+libssl-dev \
+zlib1g-dev \
+libxslt1-dev \
+libxml2-dev \
+python \
+python-pip \ 
+python-dev \
+build-essential \
+samba \
+-y
