46e73586cc7cdf22280ccb8cb5b3c4a0b05b9d91
henioStraszny <jacek.styrylski@gmail.com>
2016-07-10 13:08:29 +0200

    Update piUmbrellaInstall.sh

--- piUmbrellaInstall.sh
+++ piUmbrellaInstall.sh
@@ -1,4 +1,4 @@
 set -x
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge deluge-console deluge-web -y
+sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web -y
