d099acf440c2046ca3febb7c1e67b2033b462d65
henioStraszny <jacek.styrylski@gmail.com>
2016-07-31 18:35:22 +0200

    asda

--- piUmbrellaInstall.sh
+++ piUmbrellaInstall.sh
@@ -1,10 +1,10 @@
 ExternalDrive=/media/Biggy
 
-printf \napt-get\n
-set -x
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web -y
+# printf \napt-get\n
+# #set -x
+# sudo apt-get update
+# sudo apt-get upgrade -y
+# sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web -y
 
 printf "\n1. New dir: ~/temp\n"
 mkdir -p $ExternalDrive/temp
