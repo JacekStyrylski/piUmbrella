1703cba1a5e28324c006c1eff96231268a3bc094
henioStraszny <jacek.styrylski@gmail.com>
2016-07-31 14:30:17 +0200

    aa

--- piUmbrellaInstall.sh
+++ piUmbrellaInstall.sh
@@ -1,19 +1,23 @@
 ExternalDrive=/media/Biggy
 
+#echo \napt-get\n
 #set -x
 #sudo apt-get update
 #sudo apt-get upgrade -y
 #sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web -y
 
+echo \nNew dir temp\n
 mkdir -p $ExternalDrive/temp
 chmod 777 $ExternalDrive/temp
 rm ~/temp
 ln -s $ExternalDrive/temp ~/
 
+echo \nNew dir git\n
 mkdir -p $ExternalDrive/git
 chmod 777 $ExternalDrive/git
 rm ~/git
 ln -s $ExternalDrive/git ~/
 
+echo \nPolish TV\n
 curl -o ~/temp/repository.sd-xbmc.org-2.0.0.zip http://sd-xbmc.org/repository/repository.sd-xbmc.org/repository.sd-xbmc.org-2.0.0.zip
 
