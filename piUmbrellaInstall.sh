19d54b0cac62712ddcece43e6f49287898a9bc7a
henioStraszny <jacek.styrylski@gmail.com>
2016-07-31 13:57:04 +0200

    aaa

--- piUmbrellaInstall.sh
+++ piUmbrellaInstall.sh
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 ExternalDrive=/media/Biggy
 
 #set -x
 #sudo apt-get update
 #sudo apt-get upgrade -y
-#sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge deluge-console deluge-web -y
+#sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web -y
 
 mkdir -p $ExternalDrive/temp
 chmod 777 $ExternalDrive/temp
@@ -12,15 +11,10 @@ rm ~/temp
 ln -s $ExternalDrive/tmep ~/temp
 
 mkdir -p $ExternalDrive/git
-chmod 777 $ExternalDrive/temp
-rm ~/temp
+chmod 777 $ExternalDrive/git
+rm ~/git
 ln -s $ExternalDrive/git ~/git 
 
 rm ~/temp/http://sd-xbmc.org/repository/repository.sd-xbmc.org/repository.sd-xbmc.org-2.0.0.zip
 curl -o ~/temp/repository.sd-xbmc.org-2.0.0.zip http://sd-xbmc.org/repository/repository.sd-xbmc.org/repository.sd-xbmc.org-2.0.0.zip
-=======
-set -x
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web -y
->>>>>>> 46e73586cc7cdf22280ccb8cb5b3c4a0b05b9d91
+
