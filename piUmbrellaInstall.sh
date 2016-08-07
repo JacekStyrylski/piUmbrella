ece42e5831e4006a5ef7f90902c7ad4de6f51c34
henioStraszny <jacek.styrylski@gmail.com>
2016-08-07 14:02:03 +0200

    Added deluged to apt-get install

--- piUmbrellaInstall.sh
+++ piUmbrellaInstall.sh
@@ -4,7 +4,7 @@ printf \napt-get\n
 set -x
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web -y
+sudo apt-get install mc git libffi-dev libssl-dev zlib1g-dev libxslt1-dev libxml2-dev python python-pip python-dev build-essential samba deluge zsh deluge-console deluge-web deluged -y
 
 printf "\n1. New dir: ~/temp\n"
 mkdir -p $ExternalDrive/temp
